37ec4b66ad3c85d5ae54f323a49ec890513f345d
Aleksandar Petrovic <47416711+alek101@users.noreply.github.com>
2025-05-19 18:56:49 +0200

    form reset and validation

--- index.js
+++ index.js
@@ -8,9 +8,44 @@ document.addEventListener('alpine:init',()=>{
         bio: '',
         newsletter: true,
         showPass: false,
+        errors: {},
 
-        submitForm() {
-          console.log(this.username, this.password, this.passwordConfirm, this.belt, this.bio, this.newsletter)
-        }
+        validateForm() {
+          this.errors = {}
+
+          if(this.username.length < 3) {
+            this.errors.username = 'Username must be at leat 3 characters'
+          }
+
+          if(this.password.length < 5) {
+            this.errors.password = 'Pass must be at leat 5 characters'
+          }
+
+          if(this.password != this.passwordConfirm) {
+            this.errors.passwordConfirm = 'Password dont match'
+          }
+
+          if(!this.belt) {
+            this.errors.belt = 'Please select belt color'
+          }
+
+          if(this.bio.length < 10) {
+            this.errors.bio = 'Bio must be at leat 10 characters'
+          }
+
+
+
+        },
+
+        submitForm($event) {
+            this.validateForm()
+
+            if(Object.keys(this.errors).length === 0) {
+
+              console.log(this.username, this.password, this.passwordConfirm, this.belt, this.bio, this.newsletter)
+
+              $event.target.reset()
+            }
+          }
       }))
 })
